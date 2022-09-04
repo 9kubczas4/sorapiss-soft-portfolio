866ea25815b5b5d34c7224a0dc261fd4e2e01d4a
9kubczas4 <pawel.kubiak.dev@gmail.com>
2022-09-04 23:45:35 +0200

    feat: add module

---
+++ projects/common-ui/src/lib/components/sample-page/sample-page.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SamplePageComponent } from './sample-page.component';
+
+@NgModule({
+  declarations: [SamplePageComponent],
+  imports: [CommonModule],
+})
+export class SamplePageModule {}
