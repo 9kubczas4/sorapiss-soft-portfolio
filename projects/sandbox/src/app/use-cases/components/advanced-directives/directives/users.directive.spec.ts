1f6b57e5cd7f99ffdcb3171d3cde3cf5d1506e3a
Paweł Kubiak <43759569+9kubczas4@users.noreply.github.com>
2022-12-09 13:00:55 +0100

    Feature/ng update (#11)
    * Update peer dependencies
    
    * feat: update angular to 14
    
    * feat: upgrade angular to ng14
    
    * feat: update peer dependency
    
    * feat: upgrade ng core
    
    * fix: upgrade to 15
    
    * feat: update
    
    * feat: UI adjustments
    
    * feat: remove not used test

--- projects/sandbox/src/app/use-cases/components/advanced-directives/directives/users.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UsersDirective } from './users.directive';
-
-describe('UsersDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UsersDirective();
-    expect(directive).toBeTruthy();
-  });
-});
