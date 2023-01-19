cac333b70bda59da4440367100cfcb9390653f0a
9kubczas4 <pawel.kubiak.dev@gmail.com>
2023-01-20 00:14:37 +0100

    feat: add animation

--- projects/common-ui/src/lib/components/slideout/slideout.component.ts
+++ projects/common-ui/src/lib/components/slideout/slideout.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayModule } from '../overlay';
-import { SlideoutPosition } from './slideout.enums';
+import { SlideoutPosition, SlideoutSize } from './slideout.enum';
 import { SlideoutPositionPipe } from './slideout-position.pipe';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'lib-slideout',
@@ -10,8 +11,20 @@ import { SlideoutPositionPipe } from './slideout-position.pipe';
   templateUrl: './slideout.component.html',
   styleUrls: ['./slideout.component.scss'],
   imports: [CommonModule, OverlayModule, SlideoutPositionPipe],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('1000ms ease-in', style({ transform: 'translateX(0%)' })),
+      ]),
+      transition(':leave', [animate('1000ms ease-in', style({ transform: 'translateX(100%)' }))]),
+    ]),
+  ],
 })
 export class SlideoutComponent {
   @Input() opened = true;
   @Input() position = SlideoutPosition.RIGHT;
+  @Input() size = SlideoutSize.STANDARD;
+
+  SlideoutSize = SlideoutSize;
 }
