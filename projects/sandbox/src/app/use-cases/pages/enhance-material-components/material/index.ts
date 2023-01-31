04857422f3af978c3bff7441b9e4eea32745986c
9kubczas4 <pawel.kubiak.dev@gmail.com>
2023-01-31 20:58:30 +0100

    feat: use material table sample

--- projects/sandbox/src/app/use-cases/pages/enhance-material-components/material/index.ts
+++ projects/sandbox/src/app/use-cases/pages/enhance-material-components/material/index.ts
@@ -1,3 +1,13 @@
+import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-export const materialModules = [MatButtonModule];
+export const materialModules = [
+  MatButtonModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatProgressSpinnerModule,
+  MatTableModule,
+];
