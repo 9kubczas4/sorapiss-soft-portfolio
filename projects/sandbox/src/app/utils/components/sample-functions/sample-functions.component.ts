4e227513b16253194c3216d3b3aae461b21e20bb
9kubczas4 <pawel.kubiak.dev@gmail.com>
2023-02-18 14:22:52 +0100

    feat: log decorator

--- projects/sandbox/src/app/utils/components/sample-functions/sample-functions.component.ts
+++ projects/sandbox/src/app/utils/components/sample-functions/sample-functions.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
-import { memo } from '@ng-utils';
+import { Component, OnInit } from '@angular/core';
+import { logParametersAndReturnValue, memo } from '@ng-utils';
 
 @Component({
   selector: 'app-sample-functions',
   templateUrl: './sample-functions.component.html',
   styleUrls: ['./sample-functions.component.scss'],
 })
-export class SampleFunctionsComponent {
+export class SampleFunctionsComponent implements OnInit {
   readmeImport = import('./readme.md');
-
   /*Working sample */
   count1 = 0;
   plusOne = memo((count: number) => {
@@ -22,4 +21,13 @@ export class SampleFunctionsComponent {
     console.log(`isOdd called: `, count);
     return count % 2 === 0;
   });
+
+  ngOnInit(): void {
+    this.pow(2, 5);
+  }
+
+  @logParametersAndReturnValue
+  pow(a: number, b: number): number {
+    return Math.pow(a, b);
+  }
 }
