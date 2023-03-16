2fe70a7b3fc4aaedb7e0d5e4fa41b4d55da7696e
9kubczas4 <pawel.kubiak.1994@gmail.com>
2023-03-16 22:45:34 +0100

    feat: test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sorapiss-soft-portfolio';
+  title = "sorapiss-soft-portfolio";
 }
