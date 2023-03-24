0983e7cd99d5af65956d672c7c2ec2213e5c8bde
9kubczas4 <pawel.kubiak.dev@gmail.com>
2023-03-24 21:33:41 +0100

    feat: add default component

--- src/app/sample-use-cases/pages/change-detection-demo/change-detection-demo.module.ts
+++ src/app/sample-use-cases/pages/change-detection-demo/change-detection-demo.module.ts
@@ -6,12 +6,14 @@ import { ChangeDetectionDemoComponent } from './change-detection-demo.component'
 import { SamplePageComponent } from '@sorapiss-soft-portfolio/common-ui';
 import { components } from './components';
 import { ProcessDataPipe } from './pipes/process-data.pipe';
+import { ElementWithDefaultCdStrategyComponent } from './components/element-with-default-cd-strategy/element-with-default-cd-strategy.component';
 
 @NgModule({
   declarations: [
     ChangeDetectionDemoComponent,
     ...components,
     ProcessDataPipe,
+    ElementWithDefaultCdStrategyComponent,
   ],
   imports: [
     // vendor
